a41cd5146add39b9db558ffb9794f82ea8b2f052
vtempest <1274452+vtempest@users.noreply.github.com>
2026-06-12 00:40:52 -0700

    .

--- apps/qwksearch-api/lib/database/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/d1";
-import { getCloudflareContext } from "vinext";
-import { cache } from "react";
-import * as schema from "./schema";
-
-export const getDB = cache(() => {
-  const { env } = getCloudflareContext();
-  return drizzle(env.DB, { schema });
-});
